0437d70afd1b40df7f8f8042d99c8977c0342aee
Sourav1203 <sourav1201singh@gmail.com>
2024-10-28 12:08:57 +0530

    first commit

---
+++ numberguessing.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+    int num = 0;
+    int guess = 0;
+    int tries = 0;
+
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+
+    cout << "**** NUMBER GUESSING GAME ******\n";
+
+    do{
+        cout << "Enter a guess between (1-100): ";
+        cin >> guess;
+        tries++;
+
+        if(guess > num){
+        cout << "Too high!\n";
+        }
+        else if(guess < num){
+        cout << "Too low!\n";
+        }
+        else{
+        cout << "CORRECT! # of tries: " << tries << '\n';
+        }
+
+    }while(guess != num);
+
+    cout << "**********************************";
+
+    return 0;
+}
